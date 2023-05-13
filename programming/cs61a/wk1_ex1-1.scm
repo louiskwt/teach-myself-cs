43f7cc0bfedcd2dbd5b2f2fa27bd203526d7944a
Louis Tsang <kawingt2@gmail.com>
2023-05-13 23:31:54 +0800

    square wip

--- programming/cs61a/wk1_ex1-1.scm
+++ programming/cs61a/wk1_ex1-1.scm
@@ -7,4 +7,47 @@
 ; ans
 (/  (+ 5 4 (- 2 (- 3 (+ 6 ( / 4 5) ) ) ) ) (* 3 (- 6 2) (- 2 7)))
 
-    
+
+(define (largest a b c)
+    (define l 0)
+    (if (and (> a b) (> a c))
+      (= l a))
+    (if (and (> b c) (> b a))
+      (= l b))
+    (if (and (> c a) (> c b))
+      (= l c))
+    l 
+  )
+
+(define (smallest a b c)
+        (define s 0)
+        (if (and (< a b) (< a c))
+          (= s a))
+        (if (and (< b a) (< b c))
+          (= s b))
+        (if (and (< c a) (< c b))
+          (= s c))
+        s
+)
+
+(define (second-largest a b c)
+        (define sl 0)
+        (if (and (not (largest(a b c) a)) (not (smallest(a b c) a)))
+          (= sl a))
+        (if (and (not (largest(a b c) b)) (not (smallest(a b c) b)))
+          (= sl b))
+        (if (and (not (largest(a b c) c)) (not (smallest(a b c) c)))
+          (= sl c))
+        c
+)
+
+(define (square x)
+        (* x x)
+    )
+
+(define (square-sum x y)
+        (+ (square x) (square y))
+    )
+
+
+
