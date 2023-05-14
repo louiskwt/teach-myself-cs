ede390bb85005ef4f7dc696ab4220b6ec966a0ae
Louis Tsang <kawingt2@gmail.com>
2023-05-14 23:10:43 +0800

    solved three square sum

--- programming/cs61a/wk1_ex1-1.scm
+++ programming/cs61a/wk1_ex1-1.scm
@@ -6,7 +6,7 @@
 (/  (+ 5 4 (- 2 (- 3 (+ 6 ( / 4 5) ) ) ) ) (* 3 (- 6 2) (- 2 7)))
 
 ; three square sum ans
-(define (square x)
+define (square x)
         (* x x)
     )
 
@@ -20,7 +20,16 @@
     y
     ))
 
+(define (next-large-num curr x y z)
+        ; one arm if will not work in auto grader because it can be buggy
+        (when (and (= x curr) (or (> y z) (= y z))) y)
+        (when (and (= y curr) (or (> x z) (= x z))) x)
+        z
+    )
+
 (define (three-square-sum a b c)
-        (square-sum (>= a b) (>= b c)))
+        (define curr (>= a b))
+        (square-sum curr (next-large-num curr a b c))
+         )
 
 
