4310e85ad43cb6c1810e79ae4fe602d9f2f414d0
Louis Tsang <kawingt2@gmail.com>
2023-06-20 00:05:40 +0800

    fixed disc 02 hoc test

--- programming/cs61a/disc/disc02.py
+++ programming/cs61a/disc/disc02.py
@@ -6,8 +6,8 @@ def is_even(x):
 def keep_ints(cond, n):
     """ print out all ints 1..i..n where cond(i) is true
     >>> def is_even(x):
-            # Even numbers have remainder 0 when divided by 2
-            return x % 2 == 0
+    ...     # Even numbers have remainder 0 when divided by 2
+    ...     return x % 2 == 0
     >>> keep_ints(is_even, 5)
     2
     4
