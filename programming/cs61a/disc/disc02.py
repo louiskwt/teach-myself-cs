87cbc952c8e40b378c83d716a1895d1350d3d60f
Louis Tsang <kawingt2@gmail.com>
2023-06-21 08:35:16 +0800

    completed disc01 coding questions

--- programming/cs61a/disc/disc02.py
+++ programming/cs61a/disc/disc02.py
@@ -33,3 +33,43 @@ def make_keeper(n):
                 print(i)
     return h
 
+def print_delayed(x):
+    """
+    Return a new function. This new function, when called, will print out x and return another 
+    function with the ame behaviour
+    >>> f = print_delayed(1)
+    >>> f = f(2)
+    1
+    >>> f = f(3)
+    2
+    >>> f = f(4)(5)
+    3
+    4
+    """
+    def delay_print(y):
+        print(x)
+        return print_delayed(y)
+    return delay_print
+
+def print_n(n):
+    """
+    >>> f = print_n(2)
+    >>> f = f("hi")
+    hi
+    >>> f = f("hello")
+    hello
+    >>> f = f("bye")
+    done
+    >>> g = print_n(1)
+    >>> f = g("first")("second")("third")
+    first
+    done
+    done
+    """
+    def inner_print(x):
+        if n <= 0:
+            print("done")
+        else:
+            print(x)
+        return print_n(n - 1)
+    return inner_print
