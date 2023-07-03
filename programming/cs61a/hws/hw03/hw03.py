b45726b3a429e5bd3db21df6c24038cf0bdb4ec0
Louis Tsang <kawingt2@gmail.com>
2023-07-03 23:46:33 +0800

    hw03 composer wip

--- programming/cs61a/hws/hw03/hw03.py
+++ programming/cs61a/hws/hw03/hw03.py
@@ -21,7 +21,7 @@ def composer(func=lambda x: x):
     """
     def func_adder(g):
         "*** YOUR CODE HERE ***"
-        return composer()
+        return lambda x: g(func(x)), composer(g)
     
     return func, func_adder
 
