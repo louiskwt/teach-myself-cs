61ec8b63ef2002637a38a66804998f18e008816b
Louis Tsang <kawingt2@gmail.com>
2023-07-13 00:03:11 +0800

    passed q3

--- programming/cs61a/exams/61a-su20-mt1/q3/tests/q3.py
+++ programming/cs61a/exams/61a-su20-mt1/q3/tests/q3.py
@@ -1,16 +1,30 @@
-test = {'name': 'q3',
- 'points': 10,
- 'suites': [{'cases': [{'code': '>>> maxkd(1234, 1)\n'
-                                '4\n'
-                                '\n'
-                                '>>> maxkd(32749, 2)\n'
-                                '79\n'
-                                '\n'
-                                '>>> maxkd(1917, 2)\n'
-                                '97\n'
-                                '\n'
-                                '>>> maxkd(32749, 18)\n'
-                                '32749\n'}],
-             'scored': True,
-             'setup': 'from q3 import *',
-             'type': 'doctest'}]}
+test = {
+  'name': 'q3',
+  'points': 10,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> maxkd(1234, 1)
+          4
+          
+          >>> maxkd(32749, 2)
+          79
+          
+          >>> maxkd(1917, 2)
+          97
+          
+          >>> maxkd(32749, 18)
+          32749
+          """,
+          'hidden': False
+        }
+      ],
+      'scored': True,
+      'setup': 'from q3 import *',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
