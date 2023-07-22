001e16fe6829149dba8b9ace3ecc304a1872d4dc
Louis Tsang <kawingt2@gmail.com>
2023-07-22 09:34:49 +0800

    implemented is_prime

--- programming/cs61a/disc/disc04.py
+++ programming/cs61a/disc/disc04.py
@@ -13,3 +13,30 @@ def multiply(m, n):
     return m + multiply(m, n-1)
 
 assert multiply(6, 3) == 18
+
+def is_prime(n):
+    """
+    >>> is_prime(7)
+    True
+    >>> is_prime(10)
+    False
+    >>> is_primt(1)
+    False
+
+    """
+    if n == 1:
+        return False
+
+    def prime_helper(n, k):
+        if n % k == 0:
+            return False
+        elif k + 1 == n:
+            return True
+        else:
+            return prime_helper(n, k+1)
+    return prime_helper(n, 2)
+
+print(is_prime(7))
+print(is_prime(3))
+print(is_prime(4))
+print(is_prime(10))
