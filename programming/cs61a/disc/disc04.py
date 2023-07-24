6ecf7c8f24c46ac94c8f9c19a5c8bbf062be6d80
Louis Tsang <kawingt2@gmail.com>
2023-07-24 08:34:31 +0800

    wip count_k

--- programming/cs61a/disc/disc04.py
+++ programming/cs61a/disc/disc04.py
@@ -61,3 +61,22 @@ print(count_stair_ways(4))
 print(count_stair_ways(3))
 
 
+# Q2.2
+def count_k(n, k):
+    """
+    >>> count_k(3, 3) # 3, 2 + 1, 1 + 2, 1 + 1 + 1
+    4
+    >>> count_k(4, 4)
+    8
+    >>> count_k(10, 3)
+    274
+    >>> count_k(300, 1) # Only one step at a time
+    1
+    """
+    if n < 3:
+        return n
+    if k == 1:
+        return k
+    return count_k(n, k-1)
+
+print(count_k(3,3))
