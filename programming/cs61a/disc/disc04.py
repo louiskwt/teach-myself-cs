cce83a3811e3d03c9a6c8fde2e390b7bafb76493
Louis Tsang <kawingt2@gmail.com>
2023-07-29 18:53:26 +0800

    wip disc04 Q3.3

--- programming/cs61a/disc/disc04.py
+++ programming/cs61a/disc/disc04.py
@@ -122,3 +122,28 @@ def even_weighted(s):
 
 x = [1, 2, 3, 4, 5, 6]
 print(even_weighted(x))
+
+print("_______________End of Q3.2_______________")
+
+def max_product(s):
+    """Return the maximum product that can be formed using non-consecutive elements of s.
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+    max = 1;
+    if len(s) == 0:
+        return max
+
+    for index, element in enumerate(s):
+        if index % 2 == 0:
+            print(index, element)
+            max *= element
+
+    return max
+
+print(max_product([10,3,1,9,2]))
+
