89ad434003c83fe200436545f5114464de1d9901
Louis Tsang <kawingt2@gmail.com>
2023-08-17 07:35:49 +0800

    expanded trial data and reorganize code

--- programming/python/advent_of_code/day3_22/priority.py
+++ programming/python/advent_of_code/day3_22/priority.py
@@ -1,33 +1,42 @@
 import string
 
-try:
-    # priority dictionary
-    weight_list = [i for i in range(1, 52+1)]
-    item_list = list(string.ascii_letters)
-    priority_dict = dict(zip(item_list, weight_list))
 
-    rucksack = open("input.txt", 'r').read().splitlines()
-    rucksack_with_splited_items = [[*r] for r in rucksack]
+def sum_priority(rucksacks):
+    try:
+        # priority dictionary
+        weight_list = [i for i in range(1, 52+1)]
+        item_list = list(string.ascii_letters)
+        priority_dict = dict(zip(item_list, weight_list))
 
-    trial_data = [
-        "vJrwpWtwJgWrhcsFMMfFFhFp",
-        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
-    ]
-    trial_rucksack = [list(r) for r in trial_data]
-    trial_priority_lst = [priority_dict[item] for sublist in trial_rucksack for n, item in enumerate(sublist) if item in sublist[:n]]
+        dup = []
+        for r in rucksacks:
+            # c represents the compartments in the rucksack (r)
+            c1 = r[len(r)//2:]
+            c2 = r[:len(r)//2]
+            d = set(c1) & set(c2)
+            for i in d:
+                dup.append(priority_dict[i])
+        
+        print(sum(dup))
+    except:
+        print("An error occurred")
 
-    print(trial_priority_lst)
 
-    dup = []
-    for r in trial_rucksack:
-        # c represents the compartment in the rucksack (r)
-        c1 = r[len(r)//2:]
-        c2 = r[:len(r)//2]
-        d = set(c1) & set(c2)
-        for i in d:
-            dup.append(priority_dict[i])
-    
-    print(sum(dup))
+trial_data = [
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw",
+        ]
+trial_rucksack = [list(r) for r in trial_data]
 
-except:
-    print("Error when loading file")
+
+rucksack = open("input.txt", 'r').read().splitlines()
+rucksack_with_splited_items = [[*r] for r in rucksack]
+
+sum_priority(trial_rucksack)
+
+
+
