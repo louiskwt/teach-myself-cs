a512e35cf20056ac7c2eba214b4d14c2af12d77a
Louis Tsang <kawingt2@gmail.com>
2023-08-16 22:12:54 +0800

    generated priority list for day3

--- programming/python/advent_of_code/day3_22/priority.py
+++ programming/python/advent_of_code/day3_22/priority.py
@@ -1,7 +1,13 @@
+import string
+
 try:
+    # priority dictionary
+    weight_list = [i for i in range(1, 52+1)]
+    item_list = list(string.ascii_letters)
+    priority_dict = dict(zip(item_list, weight_list))
+
     rucksack = open("input.txt", 'r').read().splitlines()
     rucksack_with_splited_items = [[*r] for r in rucksack]
-    print(rucksack_with_splited_items)
     
 except:
     print("Error when loading file")
