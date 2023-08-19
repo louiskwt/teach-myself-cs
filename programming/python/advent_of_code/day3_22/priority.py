f5f33564a3994de722d1d83f62e4e4c21cd7e748
Louis Tsang <kawingt2@gmail.com>
2023-08-19 10:32:24 +0800

    cleaned up day3

--- programming/python/advent_of_code/day3_22/priority.py
+++ programming/python/advent_of_code/day3_22/priority.py
@@ -5,20 +5,16 @@ weight_list = [i for i in range(1, 52+1)]
 item_list = list(string.ascii_letters)
 priority_dict = dict(zip(item_list, weight_list))
 
-
 def sum_rucksack_priority(rucksacks):
-    try:
-        dup = []
-        for r in rucksacks:
-            # c represents the compartments in the rucksack (r)
-            c1 = r[len(r)//2:]
-            c2 = r[:len(r)//2]
-            d = set(c1) & set(c2)
-            for i in d:
-                dup.append(priority_dict[i])
-        print(sum(dup))
-    except:
-        print("An error occurred")
+    dup = []
+    for r in rucksacks:
+        # c represents the compartments in the rucksack (r)
+        c1 = r[len(r)//2:]
+        c2 = r[:len(r)//2]
+        d = set(c1) & set(c2)
+        for i in d:
+            dup.append(priority_dict[i])
+    print(sum(dup))
 
 def sum_badge_priority(groups):
     shared_badge_priority = []
@@ -30,10 +26,8 @@ def sum_badge_priority(groups):
 
     print(sum(shared_badge_priority))
    
-
-
-
-trial_data = [
+try:
+    trial_data = [
             "vJrwpWtwJgWrhcsFMMfFFhFp",
             "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
             "PmmdzqPrVvPwwTWBwg",
@@ -42,18 +36,19 @@ trial_data = [
             "CrZsJsPPZsGzwwsLwLmpwMDw",
         ]
 
-trial_rucksack = [list(r) for r in trial_data]
+    trial_rucksack = [list(r) for r in trial_data]
 
+    rucksack_data = open("input.txt", 'r').read().splitlines()
+    rucksacks = [list(r) for r in rucksack_data]
 
-rucksack_data = open("input.txt", 'r').read().splitlines()
-rucksacks = [list(r) for r in rucksack_data]
+    groups = [rucksack_data[i:i+3] for i in range(0, len(rucksack_data), 3)]
+    trial_group = [trial_data[i:i+3] for i in range(0, len(trial_data), 3)]
 
-groups = [rucksack_data[i:i+3] for i in range(0, len(rucksack_data), 3)]
-trial_group = [trial_data[i:i+3] for i in range(0, len(trial_data), 3)]
-
-
-sum_rucksack_priority(trial_rucksack) #157
-sum_rucksack_priority(rucksacks) #7785
-
-sum_badge_priority(trial_group) #70
-sum_badge_priority(groups) # 2633
+    #Part 1
+    sum_rucksack_priority(trial_rucksack) #157
+    sum_rucksack_priority(rucksacks) #7785
+    # Part 2
+    sum_badge_priority(trial_group) #70
+    sum_badge_priority(groups) # 2633
+except:
+    print("An error occurred")
