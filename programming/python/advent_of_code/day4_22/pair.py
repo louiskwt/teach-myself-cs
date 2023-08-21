6c7a9d6fde5b8093451af7b61cf3a862b390192a
Louis Tsang <kawingt2@gmail.com>
2023-08-21 18:10:14 +0800

    finished day4 part 1

--- programming/python/advent_of_code/day4_22/pair.py
+++ programming/python/advent_of_code/day4_22/pair.py
@@ -1,3 +1,10 @@
+def find_duplicated_pair(lst):
+    count = 0
+    for pair in lst:
+        if all(room in pair[0] for room in pair[1]) or all(room in pair[1] for room in pair[0]):
+            count += 1
+    return count
+
 try:
     assignment_list = open("input.txt", "r").read().splitlines()
     assignment_list = [group.split(",") for group in assignment_list]
@@ -5,6 +12,7 @@ try:
     for lst in assignment_list:
         section_range = [list(range(int(section.split("-")[0]), int(section.split("-")[1]) + 1)) for section in lst ]
         formatted_assignment_list.append(section_range)
-    print(formatted_assignment_list)
+
+    print(find_duplicated_pair(formatted_assignment_list)) # 560
 except:
     print("Fail to process")
