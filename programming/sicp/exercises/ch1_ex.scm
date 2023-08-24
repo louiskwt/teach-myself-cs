fedcea2d815703581b676af2bdf7ef7e0e0126e2
Louis Tsang <kawingt2@gmail.com>
2023-08-24 22:29:48 +0800

    completed ex 1.6

--- programming/sicp/exercises/ch1_ex.scm
+++ programming/sicp/exercises/ch1_ex.scm
@@ -58,3 +58,9 @@ false
 ; test 0 (p))
 ; In applicative-order evaluation, (p) will be evaluated, resulting in an infinite loop
 ; In normal-order evaluation, (p) is not evaluated because predicate of if evaluates to true
+
+; ex 1.6
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+; the problem of not using the special form if is that it will lead to an infinite loop where sqrt-iter always evaluates itself
