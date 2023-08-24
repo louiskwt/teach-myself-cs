bb3cf11f7f09d5403c58b1ec626863d2ac3d2ff5
Louis Tsang <kawingt2@gmail.com>
2023-08-24 22:10:36 +0800

    finished ex 1.5

--- programming/sicp/exercises/ch1_ex.scm
+++ programming/sicp/exercises/ch1_ex.scm
@@ -47,3 +47,14 @@ false
 ; equivalent: (+ a (abs b)) | (if (> b 0) (+ a b) (- a b)) | (+ a (if (> b 0) b (- b)))
 
 
+; ex 1.5
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+; test 0 (p))
+; In applicative-order evaluation, (p) will be evaluated, resulting in an infinite loop
+; In normal-order evaluation, (p) is not evaluated because predicate of if evaluates to true
