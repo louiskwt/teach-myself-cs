b5fb83919abd2e7b36dc4befa30e3bd70b0248ae
Louis Tsang <kawingt2@gmail.com>
2023-08-30 12:27:02 +0800

    learned about 2 types of recursions

--- programming/sicp/exercises/ch1_ex.scm
+++ programming/sicp/exercises/ch1_ex.scm
@@ -146,3 +146,19 @@ false
 ; g(n) = 2
 ; h(n) = A(n-1, A(n-1, A(n-1, ... A(n-1, 2)...)))
 ; k(n) = 5n^2
+
+; Tree recursion
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+; Tail Recursion
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
