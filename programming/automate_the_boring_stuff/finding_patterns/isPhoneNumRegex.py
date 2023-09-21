a6b4a8a3ea377ec4effe8edc3e6224ece2918e07
Louis Tsang <kawingt2@gmail.com>
2023-09-21 22:36:36 +0800

    cleaned up

--- programming/automate_the_boring_stuff/finding_patterns/isPhoneNumRegex.py
+++
@@ -1,8 +0,0 @@
-import re
-
-# creating a regex object
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-
-match_object = phoneNumRegex.search('Contact me at 415-900-3333 now.')
-
-print('Phone number found: ' + match_object.group())
