2a6463ea99d9599720e45f8a4615114e7b4892e3
Louis Tsang <kawingt2@gmail.com>
2023-11-05 11:00:59 +0800

    completed q11

--- programming/sicp/projects/hog/hog.py
+++ programming/sicp/projects/hog/hog.py
@@ -344,7 +344,11 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
 def sus_strategy(score, opponent_score, threshold=11, num_rolls=6):
     """This strategy returns 0 dice when your score would increase by at least threshold."""
     # BEGIN PROBLEM 11
-    return num_rolls  # Remove this line once implemented.
+    new_score = sus_update(0, score, opponent_score)
+    if (new_score - score) >= threshold:
+        return 0
+    else:
+        return num_rolls
     # END PROBLEM 11
 
 
