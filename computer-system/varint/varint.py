934d730ad8f19ca8dfba5f04163402e3849c5f94
Louis Tsang <kawingt2@gmail.com>
2023-11-06 22:58:54 +0800

    worked on encode function with psudocode

--- computer-system/varint/varint.py
+++ computer-system/varint/varint.py
@@ -1,5 +1,24 @@
 import struct
 
+
+def encode(n):
+    """
+    - while n > 0:
+        take lowest order 7 bits (little endian)
+        add the correct most significant bit: 1 unless final 7 bit
+        push to some sequence of bytes
+        reduce n
+    return byte sequence 
+    """
+    out = []
+    while n > 0:
+        part = n % 128 # TODO use bitmask
+        # TODO add msb
+        out.append(part)
+        n >>= 7
+    return bytes(out)
+
 # Open the test files
-with open('maxint.uint64', 'rb') as f:
-    print(struct.unpack('>Q', f.read())[0])
+with open('150.uint64', 'rb') as f:
+    n = struct.unpack('>Q', f.read())[0]
+    print(encode(n))
