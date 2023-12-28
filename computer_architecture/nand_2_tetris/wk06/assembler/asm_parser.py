3672c6854e3c104be30a9503ceecd85bf6b846bd
Louis Tsang <kawingt2@gmail.com>
2023-12-28 23:28:49 +0800

    set up command type function

--- computer_architecture/nand_2_tetris/wk06/assembler/asm_parser.py
+++ computer_architecture/nand_2_tetris/wk06/assembler/asm_parser.py
@@ -1,5 +1,8 @@
 import re
 
+A_COMMAND = 'A_COMMAND'
+C_COMMAND = 'C_COMMAND'
+L_COMMAND = 'L_COMMAND'
 
 def remove_comments_and_whitespace(code):
     if "//" in code:
@@ -14,11 +17,26 @@ def clean_line(line):
     l = remove_linebreak(line)
     return remove_comments_and_whitespace(l)
 
-def read_and_process_file(path):
+def extract_code_from_file(path):
     try:
         with open(path) as file:
             lines = [clean_line(line) for line in file.readlines()]
             code = [line for line in lines if line]
-            print(code)    
+            return code  
     except:
         print('Cannot open file. Check the file path')
+
+def command_type(code):
+    if "@" in code:
+        return A_COMMAND
+    elif "=" in code:
+        return C_COMMAND
+    else:
+        return L_COMMAND
+
+def parse(code_lines):
+    commands = []
+    # for code in code_lines:
+    #     command = command_type(code)
+    #     if command == A_COMMAND:
+    #         # symbol(command)
