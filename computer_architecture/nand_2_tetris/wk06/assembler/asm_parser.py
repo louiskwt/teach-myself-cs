c334fcb322872eeb6115862f0ec3f94af5ad5aff
Louis Tsang <kawingt2@gmail.com>
2023-12-28 23:13:45 +0800

    strip whitespace and comment in code

--- computer_architecture/nand_2_tetris/wk06/assembler/asm_parser.py
+++ computer_architecture/nand_2_tetris/wk06/assembler/asm_parser.py
@@ -1,7 +1,24 @@
+import re
+
+
+def remove_comments_and_whitespace(code):
+    if "//" in code:
+        return code.split("//")[0].strip()
+    else:      
+        return code.strip()
+
+def remove_linebreak(line):
+    return re.sub("\n", "", line)
+
+def clean_line(line):
+    l = remove_linebreak(line)
+    return remove_comments_and_whitespace(l)
+
 def read_and_process_file(path):
     try:
         with open(path) as file:
-            data = file.read()
-            print(data)        
+            lines = [clean_line(line) for line in file.readlines()]
+            code = [line for line in lines if line]
+            print(code)    
     except:
         print('Cannot open file. Check the file path')
