497fe8c742d706fb22c16d9cbc4cf2d46d799c8d
Louis Tsang <kawingt2@gmail.com>
2023-12-29 17:48:02 +0800

    wrote function to handle comp

--- computer_architecture/nand_2_tetris/wk06/assembler/asm_code.py
+++ computer_architecture/nand_2_tetris/wk06/assembler/asm_code.py
@@ -20,8 +20,42 @@ JMP_TABLE = {
     "JMP": 7,
 }
 
+COMP_TABLE = {
+    '0': 42,
+    '1': 63,
+    '-1': 58,
+    'D': 12,
+    'A': 48,
+    'M': 48,
+    '!D': 13,
+    '!A': 49,
+    '!M': 49,
+    '-D': 15,
+    '-A': 51,
+    '-M': 51,
+    'D+1': 31,
+    'A+1': 55,
+    'M+1': 55,
+    'D-1': 14,
+    'A-1': 50,
+    'M-1': 50,
+    'D+A': 2,
+    'D+M': 2,
+    'D-A': 19,
+    'D-M': 19,
+    'A-D': 7,
+    'M-D': 7,
+    'D&A': 0,
+    'D&M': 0,
+    'D|A': 21,
+    'D|M': 21,
+}
+
 def get_dest(key):
     return "{0:03b}".format(DEST_TABLE[key])
 
 def get_jmp(key):
     return "{0:03b}".format(JMP_TABLE[key])
+
+def get_comp(key):
+    return "{0:06b}".format(COMP_TABLE[key])
