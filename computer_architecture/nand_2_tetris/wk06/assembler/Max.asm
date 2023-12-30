978429621c091546d8e3a2385fa74a3f695db4d0
Louis Tsang <kawingt2@gmail.com>
2023-12-30 16:36:37 +0800

    added correct files for grader

--- computer_architecture/nand_2_tetris/wk06/assembler/Max.asm
+++ computer_architecture/nand_2_tetris/wk06/assembler/Max.asm
@@ -5,7 +5,7 @@
 
 // Computes R2 = max(R0, R1)  (R0,R1,R2 refer to RAM[0],RAM[1],RAM[2])
 
-   // D = R0 - R1
+// D = R0 - R1
    @R0
    D=M
    @R1
@@ -16,13 +16,12 @@
    // Its R1
    @R1
    D=M
-   @R2
-   M=D
-   @END
+   @SET_RESULT
    0;JMP
 (ITSR0)
-   @R0             
+   @R0
    D=M
+(SET_RESULT)
    @R2
    M=D
 (END)
