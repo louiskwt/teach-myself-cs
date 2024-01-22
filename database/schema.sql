a5a9b40d7a84270d32a833aa163db17e327e52ab
Louis Tsang <kawingt2@gmail.com>
2024-01-22 08:27:41 +0800

    in class sql

---
+++ database/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE "riders" (
+	"id" INTEGER,
+	"name" TEXT,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE "stations" (
+	"id" INTEGER,
+	"name" TEXT,
+	"line" TEXT,
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE "visits" (
+	"rider_id" INTEGER,
+	"station_id" INTEGER,
+	FOREIGN KEY("rider_id") REFERENCES "riders"("id"),
+	FOREING KEY("station_id") REFERENCES "stations"("id")
+	
+);
