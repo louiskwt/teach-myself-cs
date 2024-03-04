64759c0cdbb40f1c765b6472a081fa3d2e96b318
Louis Tsang <kawingt2@gmail.com>
2024-03-04 23:38:04 +0800

    removed C programming

--- programming/c_programming/code/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-    if(puts("Hello World!") == EOF) {
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
-}
--- programming/c_programming/code/print_hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-    printf("%s\n", "Hello Again");
-    return EXIT_SUCCESS;
-}
