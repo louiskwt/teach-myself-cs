a11d3bec98385e20b70ec93c49359ba7afc745b7
Louis Tsang <kawingt2@gmail.com>
2024-03-13 23:16:34 +0800

    completed lab01 and passed test

--- programming/cs61a_sicp/labs/lab00/lab00.py
+++ programming/cs61a_sicp/labs/lab00/lab00.py
@@ -1,9 +1,9 @@
-def twenty_twenty_three():
+def twenty_twenty_four():
     """Come up with the most creative expression that evaluates to 2023,
     using only numbers and the +, *, and - operators.
 
-    >>> twenty_twenty_three()
-    2023
+    >>> twenty_twenty_four()
+    2024
     """
-    return ______
+    return ((2 * 4) * 8 * (2 + 6)) * 4 - ((5 - 2) * 8) 
 
