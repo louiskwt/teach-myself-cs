2682400d0dc467be135b1f8dc681ba1d220a6b82
Louis Tsang <kawingt2@gmail.com>
2024-04-19 00:08:14 +0800

    finished disc04 max product

--- programming/cs61a_sicp/discs/04/disc04.py
+++ programming/cs61a_sicp/discs/04/disc04.py
@@ -52,3 +52,19 @@ def paths(m, n):
         return 1
     else:
         return paths(m-1, n) + paths(m, n-1)
+
+def max_product(s):
+    """Return the maximum product that can be formed using
+    non-consecutive elements of s.
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([])
+    1
+    """
+    if len(s) == 0:
+        return 1
+    else:
+        # start with 2 & 3
+        return s[0] * max(max_product(s[2:]), max_product(s[3:]))
