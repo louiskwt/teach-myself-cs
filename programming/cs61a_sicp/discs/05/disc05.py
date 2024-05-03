a4b999a08c5af5e7360ce9c5b4bd794a79492160
Louis Tsang <kawingt2@gmail.com>
2024-05-03 23:36:30 +0800

    transfered disc06

--- programming/cs61a_sicp/discs/05/disc05.py
+++ programming/cs61a_sicp/discs/05/disc05.py
@@ -206,20 +206,3 @@ def balanced(t):
     False
     """
     return all([sum_tree(b) == sum_tree(branches(t)[0]) for b in branches(t)] + [balanced(b) for b in branches(t)])
-
-# Q1
-# s = "cs61a"
-# s_iter = iter(s)
-# next(s_iter) --> c
-# next(s_iter) --> s
-# next(s_iter) --> 6
-
-s = [[1, 2, 3, 4]]
-i = iter(s)
-j = iter(next(i))
-# next(j) --> 1 
-# s.append(5)
-# next(i) --> 5
-# next(j) --> 2
-# list(j) --> [2,3,4]
-# next(i) --> StopIteration
