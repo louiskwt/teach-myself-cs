13fdedb091334f38239aec917bace4991f5877b6
Louis Tsang <kawingt2@gmail.com>
2024-05-04 15:26:55 +0800

    finished disc06 q3

--- programming/cs61a_sicp/discs/06/disc06.py
+++ programming/cs61a_sicp/discs/06/disc06.py
@@ -22,7 +22,7 @@ def infinite_generator(n):
         n += 1
         yield n
 
-next(infinite_generator) # Error: Type Error 'function' object is not iterable
+# next(infinite_generator) # Error: Type Error 'function' object is not iterable
 
 gen_obj = infinite_generator(1)
 next(gen_obj) # 1
@@ -52,3 +52,21 @@ school = add_prefix("schooler", ["pre", "middle", "high"])
 next(school) # preschooler
 
 list(map(lambda x: x[:-2], school)) # ["middleschool", "highschool"]
+
+def filter_iter(iterable, f):
+    """
+    Generates elements of iterable for which f returns True.
+    >>> is_even = lambda x: x % 2 == 0
+    >>> list(filter_iter(range(5), is_even)) # a list of the values yielded from the call to filter_iter
+    [0, 2, 4]
+    >>> all_odd = (2*y-1 for y in range(5))
+    >>> list(filter_iter(all_odd, is_even))
+    []
+    >>> naturals = (n for n in range(1, 100))
+    >>> s = filter_iter(naturals, is_even)
+    >>> next(s)
+    2
+    >>> next(s)
+    4
+    """
+    yield from filter(lambda x: f(x), iterable)
