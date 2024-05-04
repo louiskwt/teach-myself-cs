e42c76d7731793e1368a9e4a016f3be4c096113c
Louis Tsang <kawingt2@gmail.com>
2024-05-04 16:24:52 +0800

    finished disc06 q5

--- programming/cs61a_sicp/discs/06/disc06.py
+++ programming/cs61a_sicp/discs/06/disc06.py
@@ -87,3 +87,33 @@ def differences(it):
         return []
     else:
         yield from [it_lst[i+1] - it_lst[i] for i in range(len(it_lst)-1)]
+
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    def helper(i):
+        if i > (n ** 0.5): # Could replace with i == n
+            return True
+        elif n % i == 0:
+            return False
+        return helper(i + 1)
+    return helper(2)
+
+def primes_gen(n):
+    """Generates primes in decreasing order.
+    >>> pg = primes_gen(7)
+    >>> list(pg)
+    [7, 5, 3, 2]
+    """
+    prime_lst = []
+    while n > 1:
+        if is_prime(n):
+            prime_lst.append(n)
+        n -= 1
+    yield from prime_lst
