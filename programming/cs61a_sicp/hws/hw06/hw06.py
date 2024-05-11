136b12db696ad96832436d9ef0b5e1b5ac060059
Louis Tsang <kawingt2@gmail.com>
2024-05-11 20:05:04 +0800

    finished hw06 q3

--- programming/cs61a_sicp/hws/hw06/hw06.py
+++ programming/cs61a_sicp/hws/hw06/hw06.py
@@ -96,7 +96,10 @@ def store_digits(n):
     >>> cleaned = re.sub(r"#.*\\n", '', re.sub(r'"{3}[\s\S]*?"{3}', '', inspect.getsource(store_digits)))
     >>> print("Do not use str or reversed!") if any([r in cleaned for r in ["str", "reversed"]]) else None
     """
-    "*** YOUR CODE HERE ***"
+    rest = Link.empty
+    while n // 10 > 0 and n > 10:
+        rest, n = Link(n%10, rest), n//10
+    return Link(n, rest)
 
 
 def deep_map_mut(func, lnk):
