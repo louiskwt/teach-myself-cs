f08c7900d44d130b3f88ff00df19ec808d3cef5a
Louis Tsang <kawingt2@gmail.com>
2024-10-28 10:27:47 +0800

    completed pset 0 and tested

--- programming/mit_60001/ps0/ps0.py
+++ programming/mit_60001/ps0/ps0.py
@@ -1,7 +1,7 @@
-from numpy import log
+from numpy import log2
 
 x = int(input("Enter number x: "))
 y = int(input("Enter number y: "))
 
 print(f"x**y = {x**y}")
-print(f"log(x) = {log(x**y)}"   )
+print(f"log(x) = {int(log2(x))}")
